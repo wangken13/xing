cbb37a5379e6cdbe006ecd1246a7febd8b3639b5
ww_kk(19984023228) <3410593201@qq.com>
2026-05-26 11:59:01 +0800

    跑通单元测试

---
+++ xucheng-plus-content/xuecheng-plus-ccontent-service/src/main/java/com/xuecheng/content/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wk
+ * @since 2026-05-17
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
---
+++ xucheng-plus-content/xuecheng-plus-ccontent-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -0,0 +1,17 @@
+package com.xuecheng.content.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.po.Teachplan;
+
+/**
+ * <p>
+ * 课程计划 Mapper 接口
+ * </p>
+ *
+ * @author wk
+ * @since 2026-05-17
+ */
+public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+}
