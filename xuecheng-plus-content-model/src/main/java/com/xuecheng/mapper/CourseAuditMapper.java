dca4e73340d12565a0b20e11a39ce85c764fa239
ww_kk(19984023228) <3410593201@qq.com>
2026-06-01 21:26:35 +0800

    补充文件

---
+++ xuecheng-plus-content-model/src/main/java/com/xuecheng/mapper/CourseAuditMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.mapper;
+
+import com.xuecheng..CourseAudit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wk
+ * @since 2026-05-17
+ */
+public interface CourseAuditMapper extends BaseMapper<CourseAudit> {
+
+}
