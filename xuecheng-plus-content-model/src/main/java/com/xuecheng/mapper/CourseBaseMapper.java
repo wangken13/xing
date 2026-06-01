1b7b7838740bcee45c846b9630d330f484196452
ww_kk(19984023228) <3410593201@qq.com>
2026-06-01 21:26:58 +0800

    补充文件

---
+++ xuecheng-plus-content-model/src/main/java/com/xuecheng/mapper/CourseBaseMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.mapper;
+
+import com.xuecheng..CourseBase;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程基本信息 Mapper 接口
+ * </p>
+ *
+ * @author wk
+ * @since 2026-05-17
+ */
+public interface CourseBaseMapper extends BaseMapper<CourseBase> {
+
+}
