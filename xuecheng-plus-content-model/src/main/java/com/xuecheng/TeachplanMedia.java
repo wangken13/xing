58e402c6a7ac31391500e62778eae7f52190956e
ww_kk(19984023228) <3410593201@qq.com>
2026-06-01 21:29:19 +0800

    补充文件

---
+++ xuecheng-plus-content-model/src/main/java/com/xuecheng/TeachplanMedia.java
@@ -0,0 +1,67 @@
+package com.xuecheng.;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author wk
+ * @since 2026-05-17
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("teachplan_media")
+public class TeachplanMedia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 媒资文件id
+     */
+    private String mediaId;
+
+    /**
+     * 课程计划标识
+     */
+    private Long teachplanId;
+
+    /**
+     * 课程标识
+     */
+    private Long courseId;
+
+    /**
+     * 媒资文件原始名称
+     */
+    private String mediaFilename;
+
+    private LocalDateTime createDate;
+
+    /**
+     * 创建人
+     */
+    private String createPeople;
+
+    /**
+     * 修改人
+     */
+    private String changePeople;
+
+
+}
